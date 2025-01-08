2782dae96f0732f50304ba8d72ef9f4c322786ad
Robbin Banx <robbin-banx@users.noreply.github.com>
2025-01-08 23:11:54 +0200

    Changes to multiple functions and pytest.

--- Src/test_project.py
+++ Src/test_project.py
@@ -5,8 +5,18 @@ from project import search_base
 from project import remove_entry
 from project import database
 from project import patients_table
-import csv
 import sqlite3
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    # Code that will run before your test, for example:
+    files_before = None
+    # A test function will be run at this point
+    yield
+    # Code that will run after your test, for example:
+    files_after = clean_up()
+    assert files_after == True
 
 def test_Patient_ValueError_for_species():
 
@@ -45,12 +55,12 @@ def test_Patient_eq_False():
 def test_Patient_eq_True():
     test_patient_1=Patient("dog", "male", "Harry", "4")
     test_patient_2=Patient("dog", "male", "Harry", "4")
-    assert (test_patient_1 == test_patient_2) == True
+    assert test_patient_1 == test_patient_2
 
 
 def test_write_to_database():
     test_patient_1 = Patient("cat", "male", "Ronald", "4")
-    write_to_database(test_patient_1)
+    write_to_database(test_patient_1, silent=True)
     subjects = []
 
     # Define your search criteria (e.g., column1 = "value1")
@@ -68,27 +78,35 @@ def test_write_to_database():
             subjects.append(
                 Patient(species=i[1], gender=i[2], name=i[3], age=i[4], id=i[0])
             )
-        assert test_patient_1 not in subjects
+
+    assert test_patient_1 in subjects
+
+
+def test_search_database():
+    test_patient_1 = Patient("cat", "male", "Christian", "4")
+    write_to_database(test_patient_1, silent=True)
+    result = search_base('Christian', silent=True)
+    remove_entry(test_patient_1, silent=True)
+    assert result == test_patient_1
 
 def test_remove_entry():
-    test_patient_1 = Patient("cat", "male", "Ronald", "4")
+    test_patient_1 = Patient("cat", "male", "Evan", "4")
+    write_to_database(test_patient_1, silent=True)
     remove_entry(test_patient_1, silent=True)
-    subjects = []
 
-    # Define your search criteria (e.g., column1 = "value1")
-    search_column = "name"
+    assert search_base(test_patient_1.name) == None
 
-    with sqlite3.connect(database) as conn:
-        cursor = conn.cursor()
 
-        # Perform the search
-        query = f"SELECT * FROM {patients_table} WHERE {search_column} = ?"
-        cursor.execute(query, (test_patient_1.name,))
-        rows = cursor.fetchall()
+def clean_up():
+    delete_column = "name"  # The column used to find the specific row
+    delete_value = "Evan", "Ronald", "Christian"  # The value to match for deletion
+    for i in delete_value:
+        with sqlite3.connect(database) as conn:
+            cursor = conn.cursor()
 
-        for i in rows:
-            subjects.append(
-                Patient(species=i[1], gender=i[2], name=i[3], age=i[4], id=i[0])
-            )
+            # Delete the row from the table
+            query = f"DELETE FROM {patients_table} WHERE {delete_column} = ?"
+            cursor.execute(query, (i,))
+            conn.commit()
 
-    assert test_patient_1 not in subjects
+    return True
